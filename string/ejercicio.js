50e850ea3de7287cc81d890383e51158b6a8e017
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2025-03-23 16:17:14 -0500

    temas (estructura) de javascript

--- string/ejercicio.js
+++
@@ -1,6 +0,0 @@
-let listaNombres = ["Roberto", "Agustín","Mejía","Collazoas"];
-let nombreIngresado = prompt("Ingrese su nombre");
-let buscar = listaNombres.includes(nombreIngresado);
-alert(
-    buscar?"Sí se encuentra":"No se encuentra"
-);
